82cec46c14aaf88550aaeaec1a37260a4ed91449
amirali079 <aligoli079@gmail.com>
2022-11-04 16:06:00 +0330

    init project

---
+++ ADBProject/src/test/java/RentApp/RentApplicationTests.java
@@ -0,0 +1,13 @@
+package RentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
